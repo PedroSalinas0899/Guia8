3017bdbb078643b56972f6c46436d2bda1b01414
PedroSalinas0899 <37819466+PedroSalinas0899@users.noreply.github.com>
2020-03-20 18:58:28 -0300

    Programa finalizado 100%

--- ejercicio 1/Programa.cpp
+++ ejercicio 1/Programa.cpp
@@ -7,22 +7,49 @@
 #include "Ordenar.h"
 
 using namespace std;
-
+//Funcion principal
 int main(int argc, char *argv[]){
+    //declarar variable y obejeto a usar
     Ordenar ordenamiento = Ordenar();
-    int tamano = stoi(argv[1]);
-
-    if(tamano >= 1000000 or tamano <= 0){
-        cout << "El numero ingresado no es valido." << endl;
+    int tamano;
+    int numero_parametros = argc-1;
+    string parametro;
+    //determinar si el ingreso de parametros esta correcto
+    if(numero_parametros < 2){
+        cout << "Falta el segundo parametro." << endl;
+        cout << "Mostrar arreglos [s]." << endl;
+        cout << "No mostrar arreglos [cualquier otro parametro]." << endl;
+        return 0;
     }
-    else {
+    else{
+        cout << "Sobran pametros, sin embargo se continuara igual." << endl;
+    }
+    //bloque de expecion para determinar si el parametro 1 es valido
+    try{
+        tamano = stoi(argv[1]);
+        parametro = argv[2];
+    }
+    catch(std::invalid_argument){
+        cout << "El parametro numero ingresado no es valido." << endl;
+        return 0;
+    }
+    //determinar tamano maximo del arreglo
+    if(tamano > 1000000 or tamano <= 0){
+        cout << "El parametro numero ingresado no es valido." << endl;
+    }
+    else{
+        //declarar vector arreglo
         int *arreglo = new int[tamano];
+        //rellenarlo de numeros aleatorios
+
+        srand((unsigned)time(NULL));
 
         for(int i = 0; i < tamano; i++){
             int numero_aleatorio = (rand() % tamano) + 1;
             arreglo[i] = numero_aleatorio;
         }
-
+        //declara vector arreglos que serviran para ejecutar el ordenamiento
+        //de los 7 metodos
         int *a1= new int[tamano];
         int *a2= new int[tamano];
         int *a3= new int[tamano];
@@ -30,7 +57,7 @@ int main(int argc, char *argv[]){
         int *a5= new int[tamano];
         int *a6= new int[tamano];
         int *a7= new int[tamano];
-
+        //igualar cada arreglo al arreglo principal
         for(int j = 0; j < tamano; j++){
             a1[j] = arreglo[j];
             a2[j] = arreglo[j];
@@ -42,6 +69,8 @@ int main(int argc, char *argv[]){
         }
 
         cout << "Metodo             | Tiempo" << endl;
+        //llamamiento al objeto ordenamiento para utilizar cada metodos
+        // de ordenamiento declarado en la clase Ordenar
         ordenamiento.burbuja_menor(a1, tamano);
         ordenamiento.burbuja_mayor(a2, tamano);
         ordenamiento.insercion(a3, tamano);
@@ -50,10 +79,8 @@ int main(int argc, char *argv[]){
         ordenamiento.shell(a6, tamano);
         ordenamiento.quicksort(a7, tamano);
         cout << " " << endl;
-
-        bool ver = false;
-        string parametro = argv[2];
-
+        //verificar si el parametro "s" existe y si es asi
+        // imprimir los arreglos ordenados de cada metodo
         if (parametro == "s" ){
             cout << "Burbuja menor: ";
             ordenamiento.imprimir(a1, tamano);
